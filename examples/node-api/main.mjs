8f113160ddf93fdc86664a89151c27f6f7e6e81c
Silas Sewell <silas@userhub.com>
2024-02-21 18:18:21 -0500

    Add examples and supported verions (#3)

---
+++ examples/node-api/main.mjs
@@ -0,0 +1,54 @@
+import { AdminApi, UserApi } from "@userhub/sdk";
+
+async function main() {
+  const adminKey = process.env.ADMIN_KEY;
+  if (!adminKey) {
+    console.error("ADMIN_KEY required");
+    process.exit(1);
+  }
+
+  const userKey = process.env.USER_KEY;
+  if (!userKey) {
+    console.error("USER_KEY required");
+    process.exit(1);
+  }
+
+  const adminApi = new AdminApi(adminKey);
+
+  const res = await adminApi.users.list({ pageSize: 5 });
+
+  for (const user of res.users) {
+    if (user.disabled) {
+      continue;
+    }
+
+    const apiSession = await adminApi.users.createApiSession(user.id);
+
+    const userApi = new UserApi(userKey, apiSession.accessToken);
+
+    const session = await userApi.session.get();
+
+    console.log("User:");
+    console.log(" - ID:", session.user.id);
+    console.log(" - Name:", session.user.displayName);
+    console.log(" - Memberships:", session.memberships.length);
+
+    break;
+  }
+
+  try {
+    await adminApi.users.get("fail");
+  } catch (ex) {
+    console.log();
+    console.log(ex);
+    console.log();
+    console.log("UserHub error:");
+    console.log(" - ApiCode:", ex.apiCode);
+    console.log(" - Message:", ex.apiMessage);
+    console.log(" - Reason:", ex.reason);
+    console.log(" - Param:", ex.param);
+    console.log(" - Call:", ex.call);
+  }
+}
+
+main().catch(console.error);
