1ffb1a1cf08e90c3bd247ecdc699760689d44215
Silas Sewell <silas@userhub.com>
2024-02-21 18:47:52 -0500

    Version v0.3.2

--- src/internal/constants.ts
+++ src/internal/constants.ts
@@ -1,8 +1,8 @@
 // Code generated. DO NOT EDIT.
 
 export const API_BASE_URL = "https://api.userhub.com";
-export const USER_AGENT = "UserHub-JavaScript/0.3.1";
-export const VERSION = "0.3.1";
+export const USER_AGENT = "UserHub-JavaScript/0.3.2";
+export const VERSION = "0.3.2";
 
 export const AUTH_HEADER = "Authorization";
 export const API_KEY_HEADER = "UserHub-Api-Key";
